72bb2c8432386e20216eccbdff58e32526099387
João Vitor Marques <jvsilvam@outlook.com>
2023-01-27 17:59:42 -0300

    feat: resize control on square node

--- src/components/nodes/Square.tsx
+++ src/components/nodes/Square.tsx
@@ -1,31 +1,40 @@
+import { NodeResizer } from "@reactflow/node-resizer";
 import { NodeProps, Handle,Position } from "reactflow";
 
-export function Square(props: NodeProps) {
+export function Square({ selected }: NodeProps) {
   return (
-    <div className="bg-violet-500 rounded w-[150px] h-[100px]">
+    <div className="bg-violet-500 rounded h-full w-full min-w-[150px] min-h-[100px]">
+      <NodeResizer 
+        minHeight={100}
+        minWidth={150}
+        isVisible={selected} 
+        lineClassName="border-blue-400"
+        handleClassName="h-3 w-3 bg-white border-2 rounded border-blue-400"
+      />
+
       <Handle 
         id="right" 
         position={Position.Right} 
         type="source"
-        className="-right-4 w-2 h-2 bg-blue-400/80"  
+        className={`${selected ? '-right-4 h-2 w-2 bg-blue-400/80' : 'border-none bg-transparent'}`} 
       />
       <Handle 
         id="left" 
         position={Position.Left} 
         type="source"
-        className="-left-4 w-2 h-2 bg-blue-400/80" 
+        className={`${selected ? '-left-4 h-2 w-2 bg-blue-400/80' : 'border-none bg-transparent'}`}
       />
       <Handle 
         id="top" 
         position={Position.Top} 
         type="source"
-        className="-top-4 w-2 h-2 bg-blue-400/80" 
+        className={`${selected ? '-top-4 h-2 w-2 bg-blue-400/80' : 'border-none bg-transparent'}`} 
       />
       <Handle 
         id="bottom" 
         position={Position.Bottom} 
         type="source"
-        className="-bottom-4 w-2 h-2 bg-blue-400/80" 
+        className={`${selected ? '-bottom-4 h-2 w-2 bg-blue-400/80' : 'border-none bg-transparent'}`}
       />
     </div>
   )
